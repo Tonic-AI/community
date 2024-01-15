61672fb464a8656f728f3c4312c79457a7bdb9a2
tonic <josephp@tuta.io>
2024-01-15 18:31:59 +0100

    Removed together-cli subtree

--- together-cli/README.md
+++
@@ -1,9 +0,0 @@
-# Together-Client
-
-## Installation
-
-**Edge Version**
-
-```bash
-pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ together_cli -U
-```
--- together-cli/together_cli/src/core/render.py
+++
@@ -1,5 +0,0 @@
-def render(template, **kwargs):
-    """Render a template with the given context."""
-    for key, value in kwargs.items():
-        template = template.replace("{{"+key.upper()+"}}", value)
-    return template
