3c2ae59bcc7ca569470525274436637a51bb0144
tonic <josephp@tuta.io>
2024-01-15 18:28:49 +0100

    Removed jetson-containers subtree

--- jetson-containers/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#PYTHONPATH="$PYTHONPATH:$ROOT" python3 $ROOT/jetson_containers/build.py "$@"
-
-PYTHONPATH="$PYTHONPATH:$ROOT" python3 -m jetson_containers.build "$@"
