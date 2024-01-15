9b8b3eea569b8feff975d28ae72bf730a353c5aa
tonic <josephp@tuta.io>
2024-01-15 19:42:45 +0100

    uploading the stack using forks

--- clone_repos.sh
+++ clone_repos.sh
@@ -2,47 +2,11 @@
 
 # Define an array of repository URLs
 REPO_URLS=(
-    https://github.com/intel/ai-workflows
-    https://github.com/intel/ai-containers
-    https://github.com/Clarifai/clarifai-python
-    https://github.com/docker/compose
-    https://github.com/huggingface/diffusers
-    https://github.com/embedchain/embedchain
-    https://github.com/deepset-ai/haystack
-    https://github.com/KBVE/kbve
-    https://github.com/logspace-ai/langflow
-    https://github.com/run-llama/llama_index
-    https://github.com/mistralai/client-python
-    https://github.com/allenai/open-instruct
-    https://github.com/KillianLucas/open-interpreter
-    https://github.com/raidendotai/openv0
-    https://github.com/huggingface/optimum
-    https://github.com/replicate/replicate-python
-    https://github.com/docker/scout-cli
-    https://github.com/raidendotai/shell2-cli
-    https://github.com/google-research/t5x
-    https://github.com/togethercomputer/together-cli
-    https://github.com/huggingface/transformers
-    https://github.com/UKPLab/sentence-transformers
-    https://github.com/vercel/vercel
-    https://github.com/facebookresearch/xformers
-    https://github.com/NVIDIA/nvidia-container-toolkit
-    https://github.com/dusty-nv/jetson-containers
-    https://github.com/e2b-dev/E2B
-    https://github.com/FlowiseAI/Flowise
-    https://github.com/cpacker/MemGPT
-    https://github.com/QwenLM/Qwen-Agent
-    https://github.com/microsoft/TaskWeaver
-    https://github.com/langchain-ai/langchain
-    https://github.com/microsoft/promptflow
-    https://github.com/openai/openai-python
-    https://github.com/chroma-core/chroma
-    https://github.com/qdrant/qdrant
-    https://github.com/streamlit/streamlit
-    https://github.com/gradio-app/gradio
-    https://github.com/openai/tiktoken
-    https://github.com/google/sentencepiece
-    https://github.com/TimDettmers/bitsandbytes    
+    https://github.com/cg123/mergekit
+    https://github.com/meta-introspector/time/
+    https://github.com/meta-introspector/ai-ticket
+    https://github.com/runpod/runpod-python
+    https://github.com/facebookresearch/pytorchvideo    
 )
 
 # Directory where you want to clone the repositories
