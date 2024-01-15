fd2e74eb09a98679283d3046d6d394ac48b9e57c
tonic <josephp@tuta.io>
2024-01-15 18:26:37 +0100

    Removed ai-containers subtree

--- ai-containers/preset/classical-ml/tests/scikit/test_scikit.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-source activate classical-ml
-
-set -xe
-SCRIPT_DIR="$(dirname $0)"
-
-python ${SCRIPT_DIR}/kmeans.py
-
-python ${SCRIPT_DIR}/kmeans.py true
--- ai-containers/tensorflow/serving/half_plus_two.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip install -r requirements.txt
-python half_plus_two_saved_model.py
-sleep 5
-# REST
-python half_plus_two_client.py
-rm -rf half_plus_two
-find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
